73a20e3cb744bf12c2a2bb5cb6528189f334f7aa
Killian <killian@desktop-killian.home>
2017-10-05 19:58:31 +0200

    java Pi

--- Java/tp1/Pi.java
+++ Java/tp1/Pi.java
@@ -1,1 +1,26 @@
+import java.util.*;
+import java.lang.*;
 
+public class Pi
+{
+  public static void main(String[] args)
+  {
+    double n = Integer.parseInt(args[0]);
+    if ((args.length != 1) && (n <=0))
+    {
+      System.out.println("Fournisser une valeur en argument.");
+      return;
+    }
+
+    double tmp=0;
+
+    for (int i=1; i <= n; ++i)
+    {
+      tmp += 1/Math.pow(i,2);
+    }
+
+    double pi = Math.sqrt(6*tmp);
+
+    System.out.println("La valeur de PI approchée à "+n+" itérations : "+pi);
+  }
+}
