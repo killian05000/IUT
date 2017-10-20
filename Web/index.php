dfc882e0749984c8647ba02eb7249d7717227e3b
Killian <killian@desktop-killian.home>
2017-10-20 20:17:56 +0200

    création

--- Web/index.php
+++ Web/index.php
@@ -1,1 +1,37 @@
-
+<Doctype html>
+<html>
+	<head>
+		<title>Web.jdr</title>
+		<meta charset="utf-8" />
+		<meta name="author" content="Wolfger Killian, Barbe Bapiste"/>
+		<meta name="description" content="JDR"/>
+		<meta name="keywords" content="jeu de role, jeu, roleplay, scenario, campagne, tokens, token, projet iut, projet web, baptiste barbe, killian wolfger, projet iut kw bb"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="Css/css_file.css"/>
+	</head>
+	<header id="titreprojet">
+		<p>Jeux de rôle !</p>
+	</header>
+	<body>
+	<div id="menu">
+	<ul id="main_menu">
+		<li><a href="index.php">Acceuil</a></li>
+		<li><a href="#">Taverne</a></li>
+			<ul>
+				<li><a href="#">Histoire</a></li>
+				<li><a href="#">Ressources</a></li>
+				<li><a href="#">Recrutement</a></li>
+			</ul>
+		<li><a href="#">Journal</a>
+			<ul>
+				<li><a href="#">Nouvelle publication</a></li>
+				<li><a href="#">Historique</a></li>
+			</ul>
+		</li>
+		<li><a href="#">A propos</a>
+		</li>
+	</ul>
+	</div>
+	</body>
+</html>
