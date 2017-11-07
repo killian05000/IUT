89a93ff636ab9d91dd6eb71f184dbab8bc554368
killian05000 <killianwolfger@hotmail.fr>
2017-11-07 17:32:48 +0100

    tp2 java

--- Java/tp2/List.java
+++ Java/tp2/List.java
@@ -1,20 +1,36 @@
+import java.util.Vector;
 import java.util.*;
 
 public class List
 {
-  public static void main(String[] args)
-  {
-    List = new Vector();
-    //Scanner sc = new Scanner(System.in);
-    fillVector();
-  }
+    public static void main(String []args)
+    {
+      Vector v = new Vector();
+      //String tmp = v.elementAt(1);
+      //System.out.println(v.elementAt(1));
+      fillVector(v);
+      displayVector(v);
+    }
 
-  public static void fillVector()
-  {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Saisissez des noms : ");
-    String nom = sc.next();
-    String nom2 = sc.next();
-    System.out.println(nom+" et "+nom2);
-  }
+    public static void fillVector(Vector list)
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Combien de fois voulez vous afficher ? : ");
+      int nb = sc.nextInt();
+      for ( int i=0; i < nb; ++i)
+      {
+        System.out.print("Entrez le nom n°"+(i+1)+" : ");
+        String tmp = sc.next();
+        list.addElement(tmp);
+      }
+    }
+
+    public static void displayVector(Vector list)
+    {
+      int nb = list.size();
+      for ( int i=0; i < nb; ++i)
+      {
+        System.out.println(list.elementAt(i));
+      }
+    }
 }
