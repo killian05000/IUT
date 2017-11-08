b08dab4c223d37c3766fd6c95721a42276940a17
killian05000 <killianwolfger@hotmail.fr>
2017-11-08 15:15:37 +0100

    tp2 java

--- Java/tp2/List.java
+++ Java/tp2/List.java
@@ -5,17 +5,19 @@ public class List
 {
     public static void main(String []args)
     {
-      Vector v = new Vector();
+      Vector<String> v = new Vector<String>();
       //String tmp = v.elementAt(1);
       //System.out.println(v.elementAt(1));
       fillVector(v);
+      displayVector(v);
+      deleteDuplicates(v);
       displayVector(v);
     }
 
     public static void fillVector(Vector list)
     {
       Scanner sc = new Scanner(System.in);
-      System.out.print("Combien de fois voulez vous afficher ? : ");
+      System.out.print("Combien d'élements voulez vous entrer ? : ");
       int nb = sc.nextInt();
       for ( int i=0; i < nb; ++i)
       {
@@ -23,6 +25,7 @@ public class List
         String tmp = sc.next();
         list.addElement(tmp);
       }
+      System.out.println();
     }
 
     public static void displayVector(Vector list)
@@ -31,6 +34,24 @@ public class List
       for ( int i=0; i < nb; ++i)
       {
         System.out.println(list.elementAt(i));
+      }
+      System.out.println();
+    }
+
+    public static void deleteDuplicates(Vector list)
+    {
+      int nb = list.size();
+      for ( int i=0; i < nb; ++i)
+      {
+        String tmp = (String)list.get(i);
+        for ( int k=0; k < nb; k++)
+        {
+          String tmp2 = (String)list.get(i);
+          if ((tmp == tmp2) && (list.indexOf(tmp) != list.indexOf(tmp2)))
+          {
+            list.removeElementAt(k);
+          }
+        }
       }
     }
 }
