98c516f82c42b12ed563796932e9e913ab2d656c
Killian <killian@desktop-killian.home>
2017-11-09 23:57:00 +0100

    tp2 java

--- Java/tp2/List.java
+++ Java/tp2/List.java
@@ -7,29 +7,14 @@ public class List
     {
       Vector<String> v = new Vector<String>();
 
-      boolean test=false;
-      while (!test)
-      {
-        try
-        {
-        fillVector(v);
-        test = true;
-        }
-        catch(InputMismatchException e)
-        {
-          System.out.println();
-          System.out.println("Erreur de typage survenue !");
-          System.out.println("---------Réessayez---------");
-          System.out.println();
-        }
-      }
+      fillVector(v);
 
       System.out.println();
-      System.out.print("Liste non ordonnée : ");
+      System.out.print("Unordered list : ");
       displayVector(v);
 
       orderList(v);
-      System.out.print("Liste ordonnée : ");
+      System.out.print("Ordered list : ");
       displayVector(v);
 
       int size = v.size();
@@ -37,23 +22,42 @@ public class List
       int newSize = v.size();
       if (newSize < size)
       {
-      System.out.print("Liste sans doublon : ");
+      System.out.print("List without duplicates : ");
       displayVector(v);
       }
     }
 
     public static void fillVector(Vector list)
     {
-      Scanner sc = new Scanner(System.in);
-      System.out.print("Combien d'élements voulez vous entrer ? : ");
-      int nb = sc.nextInt();
-      System.out.println();
-      for ( int i=0; i < nb; ++i)
+      boolean test=false;
+      while (!test)
       {
-        System.out.print("Entrez le nom n°"+(i+1)+" : ");
-        String tmp = sc.next();
-        list.addElement(tmp);
+        try
+        {
+          Scanner sc = new Scanner(System.in);
+          System.out.print("How many elements do you want to enter ? ");
+          int nb = sc.nextInt();
+          if (nb!=0)
+          {
+            System.out.println();
+            for ( int i=0; i < nb; ++i)
+            {
+              System.out.print("Enter the element n°"+(i+1)+" : ");
+              String tmp = sc.next();
+              list.addElement(tmp);
+            }
+            test = true;
+          }
+        }
+        catch(InputMismatchException e)
+        {
+          System.out.println();
+          System.out.println("A data type error have been occured !");
+          System.out.println("--------------Try again--------------");
+          System.out.println();
+        }
       }
+
     }
 
     public static void displayVector(Vector list)
