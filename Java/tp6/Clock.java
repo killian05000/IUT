ac666e22f6cc1a782cc5e044235c9c5556f4ef4a
killian05000 <killianwolfger@hotmail.fr>
2017-11-30 16:26:45 +0100

    tp6 java

---
+++ Java/tp6/Clock.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Clock
+{
+  public static void main(String[] args)
+  {
+    GraphicWindow window = new GraphicWindow();
+  }
+}
---
+++ Java/tp6/GraphicWindow.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import javax.swing.JFrame;
+
+public class GraphicWindow extends JFrame
+{
+  public GraphicWindow()
+  {
+    this.setSize(300,300);
+    this.setLocationRelativeTo(null);
+    this.setResizable(false);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+  }
+}
