9e79c00c0963d8dc42cd5949fba205d246f6b230
killian05000 <killianwolfger@hotmail.fr>
2017-12-08 09:59:32 +0100

    tp1 algo

--- Algo/tp1.cpp
+++ Algo/tp1.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
-#include <ifstream>
+#include <fstream>
 #include <unordered_map>
 using namespace std;
 
+void addWord(string _content)
+{
+  if(_content n'est pas dans um)
+    um.add(_content)
+  else
+    on incremente la valeur;
+}
+
 int main()
 {
+  unordered_map<string, size_t> um;
   string fileName="Jules_Verne_Voyage_au_centre_de_la_Terre.txt";
-  ifstream fichier(fileName, ios::in);
-  if(fichier)
+  ifstream file(fileName, ios::in);
+  if(file)
   {
+    string content;
 
+    while(file.good())
+    {
+      file >> content;
+      addWord(content);
+    }
+
+    file.close();
   }
   else
-    cerr << "Impossible d'ouvrir le fichier !" << endl
+    cerr << "Impossible d'ouvrir le fichier !" << endl;
 }
