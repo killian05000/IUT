93b3c2119df3be70a29725678fad80948947df6b
killian05000 <killianwolfger@hotmail.fr>
2017-12-08 14:55:41 +0100

    tp1 algo

--- Algo/tp1.cpp
+++ Algo/tp1.cpp
@@ -3,17 +3,20 @@
 #include <unordered_map>
 using namespace std;
 
+unordered_map<string, size_t> mymap;
+//unordered_map<string, size_t>::const_iterator got = mymap.find(_content);
+
 void addWord(string _content)
 {
-  if(_content n'est pas dans um)
-    um.add(_content)
-  else
-    on incremente la valeur;
+  // if (ca y est)
+  //   j'itere le compteur de ce mot
+  // else
+  //   j'ajoute le mot
 }
 
 int main()
 {
-  unordered_map<string, size_t> um;
+  //unordered_map<string, size_t> mymap;
   string fileName="Jules_Verne_Voyage_au_centre_de_la_Terre.txt";
   ifstream file(fileName, ios::in);
   if(file)
@@ -23,6 +26,7 @@ int main()
     while(file.good())
     {
       file >> content;
+      cout << content << endl;
       addWord(content);
     }
 
