ef1f36305c1087f6364052c2a8ed76f4685cc2f5
killian05000 <killianwolfger@hotmail.fr>
2017-12-11 11:31:02 +0100

    tp7 java

--- Java/tp7/GraphicWindow.java
+++ Java/tp7/GraphicWindow.java
@@ -12,37 +12,108 @@ class GraphicWindow extends JFrame implements ActionListener
     this.setLocationRelativeTo(null);
     this.setResizable(false);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setVisible(true);
 
     JMenuBar menuBar = new JMenuBar();
-    menuBar.add(menuBar);
-    JMenu menu = new JMenu("Fihier");
+    JMenu menu = new JMenu("File");
+    JMenuItem item;
+    setJMenuBar(menuBar);
     menuBar.add(menu);
-    JMenuItem item = new JMenuItem("Quitter");
-    menu.add(item);
-    item.setActionCommand("menu_quitter");
-    item.addActionListener(this);
+
+      item = new JMenuItem("Load");
+      menu.add(item);
+      item.addActionListener(this);
+
+      item = new JMenuItem("Save");
+      menu.add(item);
+      item.addActionListener(this);
+      menu.add(new JSeparator());
+
+      item = new JMenuItem("Quit");
+      menu.add(item);
+      item.addActionListener(this);
+
+    menu = new JMenu("Color");
+    menuBar.add(menu);
+
+      item = new JMenuItem("Black");
+      menu.add(item);
+      item.addActionListener(this);
+
+      item = new JMenuItem("Red");
+      menu.add(item);
+      item.addActionListener(this);
+
+      item = new JMenuItem("Blue");
+      menu.add(item);
+      item.addActionListener(this);
+      menu.add(new JSeparator());
+
+      item = new JMenuItem("Pick");
+      menu.add(item);
+      item.addActionListener(this);
   }
 
   public void actionPerformed(ActionEvent evenement)
   {
-    if (evenement.getActionCommand().equals("menu_quitter"))
-      System.out.println("MENU QUITTER");
+              ///////// MENU FILE /////////
+
+    if (evenement.getActionCommand().equals("Load"))
+    {
+      System.out.println("MENU LOAD");
+    }
+    if (evenement.getActionCommand().equals("Save"))
+    {
+      System.out.println("MENU SAVE");
+    }
+    if (evenement.getActionCommand().equals("Quit"))
+    {
+      System.out.println("MENU QUIT");
+    }
+
+              ///////// MENU COLOR /////////
+
+    if (evenement.getActionCommand().equals("Black"))
+    {
+      System.out.println("MENU2 BLACK");
+    }
+    if (evenement.getActionCommand().equals("Red"))
+    {
+      System.out.println("MENU2 RED");
+    }
+    if (evenement.getActionCommand().equals("Blue"))
+    {
+      System.out.println("MENU2 BLUE");
+    }
+    if (evenement.getActionCommand().equals("Pick"))
+    {
+      System.out.println("MENU2 PICK");
+    }
   }
 }
 
 
-// FenetreSwing(String s)
+// import javax.swing.*;
+// import java.awt.*;
+// import java.awt.event.*;
+// class GraphicWindow extends JFrame implements ActionListener
 // {
-//   super(s);
-//   setSize(400,300);
-//   setVisible(true);
-//   JMenuBar menuBar = new JMenuBar();
-//   setJMenuBar(menuBar);
-//   JMenu menu = new JMenu("Fichier");
-//   menuBar.add(menu);
-//   JMenuItem item =new JMenuItem("Quitter");
-//   menu.add(item);
-//   item.setActionCommand("menu_quitter");
-//   item.addActionListener(this);
+// GraphicWindow(String s)
+// {
+// super(s);
+// setSize(400,300);
+// setVisible(true);
+// JMenuBar menuBar = new JMenuBar();
+// setJMenuBar(menuBar);
+// JMenu menu = new JMenu("Fichier");
+// menuBar.add(menu);
+// JMenuItem item =new JMenuItem("Quitter");
+// menu.add(item);
+// item.setActionCommand("quit");
+// item.addActionListener(this);
+// }
+// public void actionPerformed(ActionEvent evenement)
+// {
+// if(evenement.getActionCommand().equals("quit"))
+// System.out.println("MENU QUITTER");
+// }
 // }
--- Java/tp7/Principal.java
+++ Java/tp7/Principal.java
@@ -5,5 +5,6 @@ public class Principal
   public static void main(String[] agrs)
   {
     GraphicWindow window = new GraphicWindow("Window tp7");
+    window.setVisible(true);
   }
 }
