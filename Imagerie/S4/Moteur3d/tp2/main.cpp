3869c2bee080e4686a98a4406e9db232002844e7
killian05000 <killianwolfger@hotmail.fr>
2018-02-09 14:15:39 +0100

    working on generate_flock

--- Imagerie/S4/Moteur3d/tp2/main.cpp
+++ Imagerie/S4/Moteur3d/tp2/main.cpp
@@ -10,16 +10,17 @@ using namespace osg;
 using namespace osgDB;
 using namespace std;
 
-Group* scene = new Group;
-StateSet* sceneState = scene->getOrCreateStateSet();
+ref_ptr<Group> scene = new Group;
+ref_ptr<StateSet> sceneState = scene->getOrCreateStateSet();
 
 Group *generate_flock(int nb_cow, float size_x, float size_y)
 {
-	Group* flock = new Group;
+	ref_ptr<Group> flock = new Group;
+
 	for(int i=0; i< nb_cow; i++)
 	{
-		PositionAttitudeTransform* transform = new PositionAttitudeTransform();
-		Node* cow = readNodeFile("data/cow_high.3ds");
+		ref_ptr<PositionAttitudeTransform> transform = new PositionAttitudeTransform();
+		ref_ptr<Node> cow = readNodeFile("data/cow_high.3ds");
 
 		random_device generator;
     uniform_int_distribution<int> distribution1(0, size_x);
@@ -32,9 +33,9 @@ Group *generate_flock(int nb_cow, float size_x, float size_y)
   	float r = distribution3(generator);
 
 		transform->setPosition(Vec3(x,y,0));
+		transform->setAttitude(Quat(DegreesToRadians(r), Vec3(0.f, 0.f, 1.0)));
 		transform->setScale(Vec3(1, 1, 1));
 		sceneState->setMode(GL_NORMALIZE, StateAttribute::ON); // pour normaliser et retrouver les effets de lumière
-		transform->setAttitude(Quat(DegreesToRadians(r), Vec3(0.f, 0.f, 1.0)));
 
 		transform->addChild(cow);
 		flock->addChild(transform);
@@ -47,7 +48,7 @@ int main()
 	osgViewer::Viewer viewer;
 	viewer.setUpViewInWindow(100, 50, 800, 600);
 
-	generate_flock(5,30,30);
+	generate_flock(100,10,10);
 	// Node* cow = readNodeFile("data/cow_high.3ds");
   //
 	// scene->addChild(cow);
